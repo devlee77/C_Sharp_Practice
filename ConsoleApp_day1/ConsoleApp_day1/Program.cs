b0e9b5f7c582f2092689f360adce3435c159d286
devlee77 <tjdcks0928@naver.com>
2021-12-02 12:31:34 +0900

    Add C_sharp day1 part 변수, 메소드, 데이터타입, 입출력 방식에 관한 내용

---
+++ ConsoleApp_day1/ConsoleApp_day1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+
+namespace Hello
+{
+    public class Program
+    {
+        public static int plus(int a, int b)
+        {
+            Console.WriteLine("Input : {0}, {1}", a, b);
+            int result = a + b;
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 10;
+            ref int b = ref a;
+            Console.WriteLine(b);
+        }
+    }
+}
