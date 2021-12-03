0247d9d211ece7ff65eff965231de8a55486b1d4
devlee77 <tjdcks0928@naver.com>
2021-12-03 09:30:00 +0900

    Add IntegerExtentsion function 확장메소드 기능 구현

---
+++ ConsoleApp_day3/ConsoleApp_day3/IntegerExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegerExtension
+{
+    public static class IntegerExtension
+    {
+        public static int Square(this int myInt)
+        {
+            return myInt * myInt;
+        }
+
+        public static int Power(this int myInt, int exponent)
+        {
+            int result = myInt;
+            for (int i = 0; i < exponent; i++)
+                result *= myInt;
+            return result;
+        }
+
+        public static string Append(this string myString, string appendData)
+        {
+            return myString + appendData;
+        }
+    }
+
+    
+}
---
+++ ConsoleApp_day3/ConsoleApp_day3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using IntegerExtension;
+
+namespace ConsoleApp_day3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //확장메소드 - IntegerExtension
+            Console.WriteLine($"3^2 : {3.Square()}");
+            Console.WriteLine($"3^4 : {3.Power(4)}");
+            Console.WriteLine($"2^10 : {2.Power(10)}");
+
+            string a = "My name is ";
+            Console.WriteLine($"{a.Append("sungclee")}");
+        }
+    }
+}
