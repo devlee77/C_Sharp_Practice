9fdd4ef3f8a3168e0b90464411985fc3b341977d
devlee77 <tjdcks0928@naver.com>
2021-12-06 18:04:52 +0900

    Add day4

--- ConsoleApp_day4/ConsoleApp_day4/Program.cs
+++ ConsoleApp_day4/ConsoleApp_day4/Program.cs
@@ -14,9 +14,63 @@ namespace main
             Console.Write($"{value} ");
         }
 
+        static void PrintArray(System.Array array)
+        {
+            foreach (var e in array)
+                Console.Write(e);
+            Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
-            int[] scores = new int[] { 80, 74, 81, 90, 34 };
+            //배열 인덱스 설정 방식
+            /*int[] scores = new int[5];
+            scores[0] = 80;
+            scores[1] = 74;
+            scores[2] = 81;
+            scores[^2] = 90;  // 배열의 마지막-1
+            scores[^1] = 34;  // 배열의 마지막
+
+            foreach (int score in scores)
+                Console.WriteLine(score);
+
+            int sum = 0;
+            foreach (int score in scores)
+                sum += score;
+
+            int average = sum / scores.Length;
+
+            Console.WriteLine($"Average Score : {average}");*/
+
+
+
+
+
+            //배열 초기화 방법
+            /* string[] array1 = new string[3] { "안녕", "Hello", "Halo" };
+
+             Console.WriteLine("array1...");
+             foreach (string greeting in array1)
+                 Console.WriteLine($" {greeting}");
+
+             string[] array2 = new string[] { "안녕", "Hello", "Halo" };
+
+             Console.WriteLine("\narray2...");
+             foreach (string greeting in array2)
+                 Console.WriteLine($" {greeting}");
+
+             string[] array3 = { "안녕", "Hello", "Halo" };
+
+             Console.WriteLine("\narray3...");
+             foreach (string greeting in array3)
+                 Console.WriteLine($" {greeting}");*/
+
+
+
+
+
+            //array 방식 연습
+            /*int[] scores = new int[] { 80, 74, 81, 90, 34 };
             foreach (int score in scores)
                 Console.Write($"{score} ");
             Console.WriteLine();
@@ -55,7 +109,28 @@ namespace main
             int[] sliced = new int[3];
             Array.Copy(scores, 0, sliced, 0, 3);
             Array.ForEach<int>(sliced, new Action<int>(Print));
-            Console.WriteLine();
+            Console.WriteLine();*/
+
+
+
+
+
+            //배열 분활하기
+           /* char[] array = new char['Z' - 'A' + 1];
+            for (int i = 0; i < array.Length; i++)
+                array[i] = (char)('A' + i);
+
+            PrintArray(array[..]);    // 0번째부터 마지막까지
+            PrintArray(array[5..]);   // 5번째부터 끝까지
+
+            Range range_5_10 = 5..10;
+            PrintArray(array[range_5_10]); // 5번째부터 9(10-1)번째까지      
+
+            Index last = ^0;
+            Range range_5_last = 5..last;
+            PrintArray(array[range_5_last]); // 5번째부터 끝(^)까지      
+
+            PrintArray(array[^4..^1]);  // 끝에서 4번째부터 끝(^)에서 1번째까지*/
         }
     }
 }
